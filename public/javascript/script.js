e68d90f27596c56e2f3f462047ccb4994d9d9b96
Arnaldo Filho <arnaldo.mpfilho@gmail.com>
2024-06-07 20:19:56 -0300

    slider

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -22,3 +22,57 @@ function slide5(){
     document.getElementById('slideshow').src="../../public/assets/images/slide5.webp";
     setTimeout("slide1()", 3000)
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector("#slider");
+    const afterImage = document.querySelector('.image.after');
+    const handle = document.querySelector('.icon');
+
+    let isSliding = false;
+
+    handle.addEventListener('mousedown', function(e) {
+        isSliding = true;
+        document.body.style.cursor = 'ew-resize';
+    });
+
+    document.addEventListener('mouseup', function(e) {
+        isSliding = false;
+        document.body.style.cursor = 'default';
+    });
+
+    document.addEventListener('mousemove', function(e) {
+        if (isSliding) {
+            const sliderRect = slider.getBoundingClientRect();
+            let offsetX = e.clientX - sliderRect.left;
+
+            if (offsetX < 0) offsetX = 0;
+            if (offsetX > sliderRect.width) offsetX = sliderRect.width;
+
+            handle.style.left = offsetX + 'px';
+            afterImage.style.clipPath = `inset(0 ${sliderRect.width - offsetX}px 0 0)`;
+        }
+    });
+
+    slider.addEventListener('touchstart', function(e) {
+        isSliding = true;
+        document.body.style.cursor = 'ew-resize';
+    });
+
+    document.addEventListener('touchend', function(e) {
+        isSliding = false;
+        document.body.style.cursor = 'default';
+    });
+
+    document.addEventListener('touchmove', function(e) {
+        if (isSliding) {
+            const sliderRect = slider.getBoundingClientRect();
+            let offsetX = e.touches[0].clientX - sliderRect.left;
+
+            if (offsetX < 0) offsetX = 0;
+            if (offsetX > sliderRect.width) offsetX = sliderRect.width;
+
+            handle.style.left = offsetX + 'px';
+            afterImage.style.clipPath = `inset(0 ${sliderRect.width - offsetX}px 0 0)`;
+        }
+    });
+});
